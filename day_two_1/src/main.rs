05839d85ccb6880a504c2cc21bd58afa0a542610
Andre Zanellato <andre.zanellato@gmail.com>
2019-12-07 13:01:52 -0300

    Day two part 1 - ok

--- day_two_1/src/main.rs
+++ day_two_1/src/main.rs
@@ -2,33 +2,68 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input").unwrap_or_else(|_| "".to_owned());
-    let mut numbers = input
+    println!("{}", input);
+    let numbers = input
         .split(',')
-        .map(|x| x.parse().unwrap_or(99))
+        .map(|x| x.parse().unwrap_or_else(|_| 0))
         .collect::<Vec<usize>>();
-    intcode_program(&numbers);
+    println!("{:?}", numbers);
+    let new_numbers = intcode_program(&numbers);
+
+    println!("{:?}", new_numbers);
 }
 
-fn intcode_program(codes: &[usize]) -> &[usize] {
+fn intcode_program(codes: &[usize]) -> Vec<usize> {
+    let mut new_codes = codes.to_vec();
     let mut cursor = 0;
-    let mut ops = codes.get(cursor).unwrap_or(&99);
-    let mut new_codes: Vec<usize> = Vec::with_capacity(codes.len());
+    let mut ops = new_codes.get(cursor).unwrap();
     while let 1 | 2 = ops {
-        let first_pos = codes.get(cursor + 1).unwrap_or(&0);
-        let second_pos = codes.get(cursor + 2).unwrap_or(&0);
-        let store_pos = codes.get(cursor + 3).unwrap_or(&0);
-        let first_number = codes.get(*first_pos).unwrap();
-        let second_number = codes.get(*second_pos).unwrap();
-        let result = if ops == &1 {
+        let first_pos = new_codes.get(cursor + 1).unwrap();
+        let second_pos = new_codes.get(cursor + 2).unwrap();
+        let store_pos = *new_codes.get(cursor + 3).unwrap();
+        let first_number = new_codes.get(*first_pos).unwrap();
+        let second_number = new_codes.get(*second_pos).unwrap();
+        println!(
+            "Operation: {}, with numbers {} and {} from position {} and {}, store that on store_pos {}",
+            ops, first_number, second_number, first_pos, second_pos, store_pos
+        );
+        let result = if *ops == 1 {
             first_number + second_number
-        } else {
+        } else if *ops == 2 {
             first_number * second_number
+        } else {
+            0
         };
-        if let Some(elem) = new_codes.get_mut(*store_pos) {
-            *elem = result
+        if let Some(elem) = new_codes.get_mut(store_pos) {
+            *elem = result;
         }
         cursor += 4;
-        ops = codes.get(cursor).unwrap_or(&99);
+        println!("{:?}", new_codes);
+        ops = new_codes.get(cursor).unwrap();
     }
-    codes
+
+    new_codes
+}
+
+#[cfg(test)]
+mod tests {
+    use super::intcode_program;
+
+    #[test]
+    fn example_test_cases() {
+        assert_eq!(intcode_program(&[1, 0, 0, 0, 99]), [2, 0, 0, 0, 99]);
+        assert_eq!(intcode_program(&[2, 3, 0, 3, 99]), [2, 3, 0, 6, 99]);
+        assert_eq!(
+            intcode_program(&[1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]),
+            [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+        );
+        assert_eq!(
+            intcode_program(&[2, 4, 4, 5, 99, 0]),
+            [2, 4, 4, 5, 99, 9801]
+        );
+        assert_eq!(
+            intcode_program(&mut [1, 1, 1, 4, 99, 5, 6, 0, 99]),
+            [30, 1, 1, 4, 2, 5, 6, 0, 99]
+        );
+    }
 }
